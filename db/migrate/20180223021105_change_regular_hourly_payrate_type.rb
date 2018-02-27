55258a905d77d766e69f3c7855ce007f6ceaa158
john kyony <jkyony@gmail.com>
2018-02-27 10:50:36 +0200

    the fix

--- db/migrate/20180223021105_change_regular_hourly_payrate_type.rb
+++ db/migrate/20180223021105_change_regular_hourly_payrate_type.rb
@@ -1,6 +1,6 @@
 class ChangeRegularHourlyPayrateType < ActiveRecord::Migration
   def self.up
-    change_column :payroll_details, :regular_hourly_payrate, :float
+    change_column :payroll_details, :regular_hourly_payrate, :float , :precision => 2, :scale => 
   end
  
   def self.down
